86ea73699df8027d4166a2a504eafedd63e3eeda
unknown <hertzrafael@hotmail.com>
2024-09-18 12:54:11 -0300

    adding rounding parameter on history and removing round method

--- asset.py
+++ asset.py
@@ -5,7 +5,7 @@ class Asset:
 
     def __init__(self, name: str, period: str = '1mo'):
         self.asset = yf.Ticker(name)
-        self.history = self.asset.history(period).reset_index()
+        self.history = self.asset.history(period, rounding=True).reset_index()
         self._transform_history()
 
     def get_history(self):
@@ -19,17 +19,12 @@ class Asset:
         )
 
         self._add_percentage(data, columns=['diff_percentage', 'diff_sum'])
-        self._round_columns(data, columns=['open', 'close'])
 
         return data
 
     def _transform_history(self):
         self.history.columns = self.history.columns.str.lower()
         self.history['date'] = to_datetime(self.history['date'].dt.strftime('%Y-%m-%d'))
-
-    def _round_columns(self, data_frame: DataFrame, columns: list[str], decimal_places: int = 2):
-        for column in columns:
-            data_frame[column] = data_frame[column].map(lambda x: round(x, decimal_places))
 
     def _add_percentage(self, data_frame: DataFrame, columns: list[str]):
         for column in columns:
